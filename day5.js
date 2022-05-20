57c236fcdadf82bbeafd7c14f102cd51bad502f2
StrongCatto <george.gio97@gmail.com>
2022-05-20 15:18:53 +0800

    http included

--- day5.js
+++ day5.js
@@ -1,4 +1,26 @@
-const colors = require('colors')
+const http = require('http');
+const PORT = 3000;
 
-console.log(colors.red('Hello World'))
-console.log(colors.yellow('Hello Earth'))
+const server = http.createServer((req, res) => {
+    if(req.url === '/product' && req.method === 'POST') {
+        res.writeHead(201, {'Content-Type': 'application/json'});
+        return res.end(JSON.stringify({
+            msg :'Product added!'
+      }));
+
+    } else if (req.url === '/product' && req.method === 'GET') {
+        res.writeHead(201, {'Content-Type': 'application/json'});
+        return res.end(JSON.stringify({
+            msg :  'list~~~~~'
+        }));
+    }
+
+    res.writeHead(404, {'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        msg :'Route not found'
+    }));
+});
+server.listen(PORT, () => {
+    console.log('Server Listening on Port :', PORT);
+
+});
